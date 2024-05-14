1b14b063d9950b66555db9794d95342cb2286ccc
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-14 22:00:05 +0700

    cell00->ex04 use -n and -z instead of comparing to a blank string

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ "$1" = "" ]
+# length of $1 is zero
+if [ -z "$1" ]
 then
     echo "No arguments supplied"
     exit
@@ -8,12 +9,14 @@ fi
 
 echo "$1"
 
-if [ "$2" != "" ]
+# length of $2 is NOT zero
+if [ -n "$2" ]
 then
     echo "$2"
 fi
 
-if [ "$3" != "" ]
+# length of $3 is NOT zero
+if [ -z "$3" ]
 then
     echo "$3"
 fi
