e4396e533f8f79561c151abb5e6a435e38b5c6b9
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-14 22:30:12 +0700

    revert cell00->ex04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# length of $1 is zero
-if [ -z "$1" ]
+if [ "$1" = "" ]
 then
     echo "No arguments supplied"
     exit
@@ -9,14 +8,12 @@ fi
 
 echo "$1"
 
-# length of $2 is NOT zero
-if [ -n "$2" ]
+if [ "$2" != "" ]
 then
     echo "$2"
 fi
 
-# length of $3 is NOT zero
-if [ -z "$3" ]
+if [ "$3" != "" ]
 then
     echo "$3"
 fi
