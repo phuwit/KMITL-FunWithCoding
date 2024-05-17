3e2457fb64629c75d990234bf1871062c2de58a5
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2024-05-17 17:14:39 +0700

    cell03->ex04:ex03bis jquerified

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,65 +1,66 @@
 const COOKIE_NAME = 'ft_list';
 const COOKIE_OPTIONS = `; expires=${new Date(2147483647 * 1000).toUTCString()};`
 
-const newButton = document.getElementById('newButton');
-const todoContainer = document.getElementById('ft_list');
-
-loadTodosFromCookie();
-addClickDetector();
-
-newButton.addEventListener("mouseup", () => {
-  const title = window.prompt('Todo title:');
-  addTodoToCookie(title);
-  loadTodosFromCookie();
-  addClickDetector();
-});
-
-function addTodoToCookie(title) {
-  const cookies = document.cookie.split("; ");
-  const cookieKey = `${COOKIE_NAME}=`
-  cookies.forEach(cookie => {
-    if (cookie.startsWith(cookieKey)) {
-      const cookieValue = cookie.replace(cookieKey, '');
-      // console.log(cookieValue);
-      document.cookie = `${cookieKey}<div>${title}</div>${cookieValue}${COOKIE_OPTIONS}`
-    }
-  });
-}
-
-function loadTodosFromCookie() {
-  const cookies = document.cookie.split("; ");
-  const cookieKey = `${COOKIE_NAME}=`
-  cookies.forEach(cookie => {
-    if (cookie.startsWith(cookieKey)) {
-      const cookieValue = cookie.replace(cookieKey, '');
-      console.log(cookieValue);
-      todoContainer.innerHTML = cookieValue;
-    }
-  });
-
-  console.log(todoContainer.childNodes)
-
-  if (todoContainer.childNodes.length === 0) {
-    document.cookie = `${cookieKey}${COOKIE_OPTIONS}`;
-  }
-}
-
-function addClickDetector() {
-  todoContainer.childNodes.forEach(node => {
-    node.addEventListener('mouseup', () => {
-      if(!window.confirm(`remove "${node.textContent}?"`)) {
-        return;
+$(document).ready(function(){
+  function addTodoToCookie(title) {
+    const cookies = document.cookie.split("; ");
+    const cookieKey = `${COOKIE_NAME}=`
+    cookies.forEach(cookie => {
+      if (cookie.startsWith(cookieKey)) {
+        const cookieValue = cookie.replace(cookieKey, '');
+        document.cookie = `${cookieKey}<div>${title}</div>${cookieValue}${COOKIE_OPTIONS}`
       }
-      
-      node.remove();
+    });
+  }
 
-      const cookies = document.cookie.split("; ");
-      const cookieKey = `${COOKIE_NAME}=`
-      cookies.forEach(cookie => {
-        if (cookie.startsWith(cookieKey)) {
-          document.cookie = `${cookieKey}${todoContainer.innerHTML}${COOKIE_OPTIONS}`
+  function loadTodosFromCookie() {
+    const cookies = document.cookie.split("; ");
+    const cookieKey = `${COOKIE_NAME}=`
+    cookies.forEach(cookie => {
+      if (cookie.startsWith(cookieKey)) {
+        const cookieValue = cookie.replace(cookieKey, '');
+        console.log(cookieValue);
+        $("#ft_list").html(cookieValue);
+      }
+    });
+
+    console.log($("#ft_list").children())
+
+    if ($("#ft_list").children().length === 0) {
+      document.cookie = `${cookieKey}${COOKIE_OPTIONS}`;
+    }
+  }
+
+  function addClickDetector() {
+    $("#ft_list").children().each(function () {
+      this.addEventListener('mouseup', () => {
+        if(!window.confirm(`remove "${this.textContent}?"`)) {
+          return;
         }
+
+        this.remove();
+
+        const cookies = document.cookie.split("; ");
+        const cookieKey = `${COOKIE_NAME}=`
+        cookies.forEach(cookie => {
+          if (cookie.startsWith(cookieKey)) {
+            document.cookie = `${cookieKey}${$("#ft_list").html()}${COOKIE_OPTIONS}`
+          }
+        });
       });
     });
+  }
+
+  loadTodosFromCookie();
+  addClickDetector();
+
+  $("#newButton").mouseup(function(){
+    const title = window.prompt('Todo title:');
+    if (!title) {
+      return
+    }
+    addTodoToCookie(title);
+    loadTodosFromCookie();
+    addClickDetector();
   });
-}
+});
